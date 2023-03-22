d24f9602e034eb93d962459374b94b3d5adaf3a0
Islam Kamal <islamkamalx1>
2023-03-22 14:52:27 +0200

    complete Home page

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const loginContainer = document.querySelector(".login-form-container");
 const closeLogin = document.querySelector("#close-btn-login");
 const toggleMenu = document.querySelector(".toggle-menu");
 const headerIcons = document.querySelector(".header__icons");
+const bestSellerBoxes = document.querySelectorAll(".best-seller__box");
+const filterList = document.querySelectorAll(".filter-list li");
 
 login.addEventListener("click", () => {
   loginContainer.classList.toggle("active");
@@ -21,3 +23,25 @@ document.addEventListener("click", (e) => {
 toggleMenu.addEventListener("click", () => {
   headerIcons.classList.toggle("active");
 });
+
+filterList.forEach((li) => {
+  li.addEventListener("click", toggleActive);
+  li.addEventListener("click", showBoxes);
+});
+
+function toggleActive(e) {
+  filterList.forEach((li) => {
+    li.classList.remove("active");
+    e.target.classList.add("active");
+  });
+}
+
+function showBoxes() {
+  bestSellerBoxes.forEach((box) => {
+    box.style.display = "none";
+  });
+
+  document.querySelectorAll(`.${this.dataset.prod}`).forEach((el) => {
+    el.style.display = "flex";
+  });
+}
